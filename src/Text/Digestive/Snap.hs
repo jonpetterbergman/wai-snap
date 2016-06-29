e839febcd2f93b7dc88cebe294a12b761f737de5
Bowl <bowl@boka-pass.se>
2016-06-29 12:38:41 +0400

    new queryFormEnv

--- src/Text/Digestive/Snap.hs
+++ src/Text/Digestive/Snap.hs
@@ -7,17 +7,24 @@ module Text.Digestive.Snap where
 import Snap.Core (MonadSnap,
                   getRequest,
                   rqMethod)
+import Data.ByteString(ByteString)  
 import Data.Text (Text)
+import Data.List(isPrefixOf)
+import Data.Maybe(mapMaybe)
+import Data.Text.Encoding(decodeUtf8With)
+import Data.Text.Encoding.Error(lenientDecode)
 import Text.Digestive.Form(Form)
 import Text.Digestive.Form.Internal(FormTree(..))
 import Text.Digestive.Form.List(DefaultList)
+import Text.Digestive.Types(Path,Env,FormInput(..),toPath)
 import Text.Digestive.View(View,postForm,getForm)
 import Network.Wai(queryString)
 import Network.Wai.Digestive(bodyFormEnv_,queryFormEnv)
 import Control.Monad.Trans.Resource(ResourceT)
 import Control.Monad.Trans(lift,liftIO)
 import Network.HTTP.Types.Method(StdMethod(..))
-import Network.HTTP.Types.QueryLike(QueryLike())
+import Network.HTTP.Types.QueryLike(QueryLike(..))
+import Network.HTTP.Types.URI(Query)
 
 runForm :: MonadSnap m 
         => Text
@@ -37,7 +44,7 @@ runFormGet :: MonadSnap m
 runFormGet name frm = 
   do
     r <- lift $ getRequest
-    let env = queryFormEnv $ queryString r
+    let env = queryFormEnv_ $ queryString r
     act <- env []
     liftIO $ print act
     case rqMethod r of
@@ -53,3 +60,14 @@ mapFM f (Map t ft) = Map (\x -> f $ t x) (mapFM f ft)
 mapFM f (Monadic act) = Monadic $ f (fmap (mapFM f) act)
 mapFM f (List dl ft) = List (fmap (mapFM f) dl) (mapFM f ft)
 mapFM f (Metadata md ft) = Metadata md $ mapFM f ft
+
+-- | Build an 'Text.Digestive.Types.Env' from a query
+queryFormEnv_ :: (QueryLike q, Monad m) => q -> Env m
+queryFormEnv_ qs = \pth ->
+  return $ map (TextInput . decodeUtf8With lenientDecode) $ matchPath pth qs'
+  where
+    qs' = toQuery qs
+    
+matchPath :: Path -> Query -> [ByteString]    
+matchPath pth = mapMaybe snd . filter go
+  where go (key,val) = pth `isPrefixOf` (toPath $ decodeUtf8With lenientDecode key)
